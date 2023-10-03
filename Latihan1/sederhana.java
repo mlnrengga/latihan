9b414a7c948ecb0984e4d2292f16668b9110fa94
Zephyrif <rifdamusyaffa007@gmail.com>
2023-10-03 16:23:54 +0700

    coba coba

---
+++ Latihan1/sederhana.java
@@ -0,0 +1,14 @@
+public class sederhana {
+    public static void main (String[] args) {
+        int a = 10, b = 20;
+        if (a > b) {
+            System.out.println("salah");
+        } else if (a < b) {
+            System.out.println("benar");
+        } else if (a == b) {
+            System.out.println("salah");
+        } else {
+            System.out.println("gaje");
+        }
+    }
+}
