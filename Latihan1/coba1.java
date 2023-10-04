016f7f0a6e235d38db6fc7ea6725e3652e3ca367
mlnrengga <mlnrengga@gmail.com>
2023-10-04 14:15:36 +0700

    TES3

--- Latihan1/coba1.java
+++ Latihan1/coba1.java
@@ -1,6 +1,12 @@
 public class coba1 {
     public static void main(String[] args) {
+       
+         System.out.println("hello");
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello"); 
+         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
